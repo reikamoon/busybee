75e5b33d89e052cff88dc7b732f500ccb73b9bde
Anjelica <ambrosio.anjelica@gmail.com>
2019-12-29 14:13:34 -0800

    set up basic foundations

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+import json
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+import os
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/busybee')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+busybee = db.busybee
+shopping = db.busybee.shopping
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    #Home
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
