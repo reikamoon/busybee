1ff4fa0edc57996f2b0222899fc8fbea980ac199
Anjelica <ambrosio.anjelica@gmail.com>
2020-01-08 16:58:10 -0800

    created dictionary so user can add products onto shopping list

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from bson.objectid import ObjectId
 from pymongo import MongoClient
 import os
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/busybee')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/bees')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
-bees = db.bees
 lists = db.lists
 products = db.products
 
@@ -27,9 +26,17 @@ def todo():
 @app.route('/mylists')
 def view_lists():
     #Show my shopping lists from db.lists
-    return render_template('list_view.html', lists=lists)
+    return render_template('Ashow_shoppinglist.html', lists=lists.find())
 
-@app.route('/mylists/new')
+@app.route('/<list_id>', methods=['GET', 'POST'])
+def show_list(list_id):
+    #Show List
+    list = lists.find_one({'_id': ObjectId(list_id)})
+    print(list)
+    return render_template('list_view.html', list=list)
+
+
+@app.route('/mylists/new/list')
 def new_list():
     #Create a New List
     return render_template('Anew_shoppinglist.html', lists={}, title='New List')
@@ -39,7 +46,7 @@ def submit_list():
     #Submit a new list to the db.lists.
     new_list = {
     'name': request.form.get('name'),
-    'products': #Insert a product from the database into list?
+    'products': request.form.get('products')
     }
     lists_id = lists.insert_one(new_list).inserted_id
     return redirect(url_for('view_lists', lists_id=lists_id))
@@ -47,20 +54,48 @@ def submit_list():
 @app.route('/edit/<list_id>', methods = ['POST'])
 def edit_list():
     #edit my shopping list
-    my_list = lists.find_one({'_id':ObjectId(list_id)})
-    return render_template('Aedit_shoppinglist.html', lists=my_list)
+    product_list = lists.find_one({'_id': ObjectId(list_id)})['products']
+    return render_template('Aedit_shoppinglist.html', lists=product_list)
 
 @app.route('/edit/<list_id>', methods=['POST'])
 def list_update(lists_id):
     #Save Edits to list and Update list in the database.
     updated_list = {
         'name': request.form.get('name'),
-        'products': #Insert a product from the database?
+        'products': request.form.get('products')
     }
     lists.update_one(
         {'_id': ObjectId(list_id)},
         {'$set': updated_list})
     return redirect(url_for('view_lists', list_id=list_id))
+
+
+
+# products
+@app.route('/mylists/<list_id>/new')
+def new_product(list_id):
+    #Create a new product
+    list = lists.find_one({'_id': ObjectId(list_id)})
+    return render_template('Bnew_product.html', list=list, products={})
+
+@app.route('/mylists/<list_id>', methods=['POST'])
+def submit_product(list_id):
+    #Submit Product to Database and add to list
+    product_list = lists.find_one({'_id': ObjectId(list_id)})['products']
+    print(product_list)
+    new_product = {
+        'name': request.form.get('name'),
+        'price': request.form.get('price'),
+        'URL': request.form.get('url'),
+        'image_url': request.form.get('image_url')
+    }
+    print(new_product)
+    product_list.append(new_product)
+    # product_id = products.insert_one(new_product).inserted_id
+    lists.update_one(
+    {'_id': ObjectId(list_id)},
+    {'$set': {'products': product_list}})
+    return redirect(url_for('show_list', list_id=list_id))
 
 
 if __name__ == '__main__':
